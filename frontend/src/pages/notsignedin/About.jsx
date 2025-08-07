e0baffa1fb7756edf97e42419f7827c639ca29c6
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2025-08-06 21:48:16 -0400

    Update team member name in About component

--- frontend/src/pages/notsignedin/About.jsx
+++ frontend/src/pages/notsignedin/About.jsx
@@ -176,7 +176,7 @@ function About() {
                   "https://images.unsplash.com/photo-1638803040283-7a5ffd48dad5?q=80&w=1674&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
               },
               {
-                name: "Jason Derulo",
+                name: "Jason Mai",
                 role: "Electrical Engineer",
                 bio: "Can solve a Rubik’s Cube in under a minute. 🧩",
                 link: "https://github.com/Jamai-i",
